ab7a35365c0cf824702ebe48620ef6de377a2633
Kishan Patel <kvpatel6351665793@gmail.com>
2026-04-18 02:29:33 +0530

    add env config , mongoDB connection and initial API route

--- pos-backend/src/app.js
+++ pos-backend/src/app.js
@@ -1,13 +1,15 @@
 import express from "express"
 import cors from "cors"
-
+import router from "./routes/testRoutes.js";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/api",router)
 
 app.get("/",(req,res)=>{
     res.send("API is runnig...")
 });
+
 
 export default app;
--- pos-backend/src/server.js
+++ pos-backend/src/server.js
@@ -1,6 +1,11 @@
 import app from "./app.js";
+import dotenv from "dotenv"
+import { connectDB } from "./config/db.js";
+dotenv.config();
 
-const PORT = 5000;
+connectDB();
+const PORT = process.env.PORT||5000;
+
 
 app.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`)    
