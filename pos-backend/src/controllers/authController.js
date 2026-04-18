5de8bdbdde1ecb118b13b50a31535a82549d5641
Ruchika <ruchika6004@gmail.com>
2026-04-18 12:54:10 +0530

    Added auth (JWT + RBAC) and product CRUD APIs

---
+++ pos-backend/src/controllers/authController.js
@@ -0,0 +1,57 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role: role || "cashier",
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // check user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // create token
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- pos-backend/src/routes/testRoutes.js
+++ pos-backend/src/routes/testRoutes.js
@@ -1,9 +1,21 @@
-import express from "express"
+import express from "express";
+import { protect, authorizeRoles } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/test",(req,res)=>{
-    res.json({message:"Backend Working !"})
-})
+// ✅ Public route (no login required)
+router.get("/test", (req, res) => {
+  res.json({ message: "Backend Working !" });
+});
 
-export default router
+// ✅ Only logged-in users
+router.get("/user", protect, (req, res) => {
+  res.json({ message: "User access granted" });
+});
+
+// ✅ Only admin allowed
+router.get("/admin", protect, authorizeRoles("admin"), (req, res) => {
+  res.json({ message: "Admin access granted" });
+});
+
+export default router;
