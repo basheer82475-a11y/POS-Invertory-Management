a9dee38f1e49ae6295381189a23661e910074ca8
Kishan Patel <kvpatel6351665793@gmail.com>
2026-04-21 03:17:43 +0530

    add admin/user auth and routing setup

--- pos-frontend/src/pages/login.jsx
+++ pos-frontend/src/pages/login.jsx
@@ -1,6 +1,7 @@
 // src/pages/Login.jsx
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios"
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -8,17 +9,31 @@ export default function Login() {
 
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    // simple check (temporary)
-    if (email === "basheer82475@gmail.com" && password === "Basheer@123") {
-      navigate("/dashboard"); // go to dashboard
-    } else
-      
-       {
-      navigate("/pos");
-    }
-  };
+ const handleLogin = async () => {
+  try {
+    const res = await axios.post("http://localhost:5000/api/auth/login", {
+      email,
+      password,
+    });
 
+    console.log("Login Successful", res.data);
+
+   
+    const user = res.data.user;
+
+    // optional: token save kar
+    localStorage.setItem("token", res.data.token);
+
+    if (user.role === "admin") {
+      navigate("/dashboard");
+    } else {
+      navigate("/products");
+    }
+
+  } catch (error) {
+    console.log(error.response?.data);
+  }
+};
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="bg-white p-6 rounded shadow w-80">
