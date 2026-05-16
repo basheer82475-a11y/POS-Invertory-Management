54d7e3d18a6631d3a6a70907c997796f97e2851e
POS-INVENTORY-MANAGEMENT <basheer82475@gmail.com>
2026-05-16 16:44:53 +0530

    used await

--- pos-backend/src/server.js
+++ pos-backend/src/server.js
@@ -1,7 +1,7 @@
-import app from "./app.js";
 import dotenv from "dotenv"
-import { connectDB } from "./config/db.js";
 dotenv.config();
+import app from "./app.js";
+import { connectDB } from "./config/db.js";
 
 connectDB();
 const PORT = process.env.PORT||5000;
